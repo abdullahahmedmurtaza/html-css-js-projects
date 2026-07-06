b6dcb37b398caad5b9625b75bcc56bca10439f1b
abdullahahmedmurtaza <abdullah.ahmed.murtaza2004@gmail.com>
2026-07-06 08:37:20 -0700

    Add delete button

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,22 +1,28 @@
-const todoList = ['wash dishes','make dinner'];
-function renderHTML(){
+const todoList = [];
+function renderTodo(){
     let todoListHTML = '';
-    for (let i = 0; i < todoList.length; i++){
-        const todo = todoList[i];
-        html = `<p>${todo}</p>`;
-        todoListHTML += html;
+    let html = '';
+    for(let i=0; i<todoList.length; i++){
+        todo = todoList[i];
+        html += `
+        <p>
+            ${todo}
+            <button onclick=
+                'todoList.splice(${i},1);
+                renderTodo();'>
+                Delete
+            </button>
+        </p>
+        `
+        todoListHTML = html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    console.log(todoListHTML);
 }
-
-console.log(todoListHTML);
-document.querySelector('.js-todo-div').innerHTML = todoListHTML;
-}
-
-
 function addTodo(){
     const inputElement = document.querySelector('.js-todo-input');
-    const name = inputElement.value;
-    todoList.push(name);
+    todoList.push(inputElement.value);
     console.log(todoList);
+    renderTodo();
     inputElement.value = '';
-    renderHTML();
 }
