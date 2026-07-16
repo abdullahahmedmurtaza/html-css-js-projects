f8179e5ce452ea5122181db7a805190f278e1a0a
abdullahahmedmurtaza <abdullah.ahmed.murtaza2004@gmail.com>
2026-07-16 07:00:07 +0500

    Build UI except dashboard+history

---
+++ weather-dashboard/script.js
@@ -0,0 +1,6 @@
+// console.log('Hello');
+
+
+const URL = `https://api.openweathermap.org/data/4.0/onecall/current?lat=${}&lon=${}&appid=${}`;
+
+const searchButtonElement = document.querySelector('.js-search-button');
