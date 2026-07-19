4a42efa1b4c5bf0ee41141c3ab2d6be7dc48cc4f
abdullahahmedmurtaza <abdullah.ahmed.murtaza2004@gmail.com>
2026-07-19 14:13:59 +0500

    Fix imports and Generate Order Summary dynamically

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,7 +1,16 @@
-export const cart = [];
+export const cart = [
+  {
+    'productId' : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    'productQuantity' : 2
+  },
+  {
+    'productId' : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    'productQuantity' : 1
+  }
+];
 
 // Add a function to push the matching item in the cart.
-function addToCart(productId, productQuantity) {
+export function addToCart(productId, productQuantity) {
   let matchingItem;
   cart.forEach((cartItem) => {
     if (cartItem.name === productId) matchingItem = cartItem;
--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 // Add a function to calculate the cart quantity
