67f64983ac9f9a2ea533035c4acf3c843e37238c
abdullahahmedmurtaza <abdullah.ahmed.murtaza2004@gmail.com>
2026-07-19 15:22:20 +0500

    Separate the currencyConverter function and Fix the radio buttons.

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { convertCurrency } from "./utils/money.js";
 
 // Add a function to calculate the cart quantity
 
@@ -49,7 +50,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            ${convertCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
