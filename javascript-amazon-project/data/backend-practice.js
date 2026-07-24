88b963ad6b72c7c2620ba4b9e8bc516e41aa00ea
abdullahahmedmurtaza <abdullah.ahmed.murtaza2004@gmail.com>
2026-07-24 11:25:57 +0500

    Intro to backend

---
+++ javascript-amazon-project/data/backend-practice.js
@@ -0,0 +1,19 @@
+const xhr = new XMLHttpRequest;
+// xhr.open('GET','https://supersimplebackend.dev');
+// xhr.addEventListener('load',()=>{
+//     console.log(xhr.response); 
+// });
+// xhr.send();
+// We can also change the response using URL paths --> extra part after the URL.
+// Backend supports only a set of URL paths
+
+// xhr.open('GET', 'https://supersimplebackend.dev/products/first');
+xhr.open('GET', 'https://supersimplebackend.dev/products/not-supported');
+xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+})
+xhr.send();
+
+// All available URL paths are almost never revealed for security reasons, however there is a simple documentation page for all the useful ones related to a backend.
+
+// A list of all available backend URLs is known as an API --> Application Programming Interface
