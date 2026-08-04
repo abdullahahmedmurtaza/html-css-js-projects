733e15ec58f097499d38dfa5207dfa8372cf5906
abdullahahmedmurtaza <abdullah.ahmed.murtaza2004@gmail.com>
2026-08-04 23:34:56 +0500

    Create function to fetch info based on city name via direct geocoding API.

--- weather-dashboard/script.js
+++ weather-dashboard/script.js
@@ -1,18 +1,23 @@
-// console.log('Hello');
+async function getLongLat(cityName){
+    try{
+        throw 'error1';
+        await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${1}&appid=63f097efdb8310eeb66d1514089df0bc`).then((response)=>{
+         return response.json();
+    });
+    }catch(error){
+        console.log(error);
+    }
+}
 
-// const URL = `https://api.openweathermap.org/data/4.0/onecall/current?lat=${}&lon=${}&appid=${}
-// const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{},{}&limit={}&appid={}`
-// `;
-
-
-
-
+getLongLat('London');
 
 
 const searchButtonElement = document.querySelector('.js-dashboard-search-button');
 searchButtonElement.addEventListener('click',()=>{
+ console.log('clicked');
  
 });
-
-
 const deleteHistoryButtonElement = document.querySelector('.js-delete-history-button')
+deleteHistoryButtonElement.addEventListener('click',()=>{
+    console.log('history deleted');
+});
